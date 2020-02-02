a98c07ad066cb4a3ff51c5d175d699fe6c619c6c
Michael <Michael@192.168.2.39>
2020-02-01 20:06:41 -0500

    - Contructor for Elevator SubSystem

--- Group9_Iteration1/src/iteration1/ElevatorSubsystem.java
+++ Group9_Iteration1/src/iteration1/ElevatorSubsystem.java
@@ -5,18 +5,21 @@ package iteration1;
 import java.io.*;
 import java.math.*;
 import java.net.*;
-import.java.util.*;
-private BlockingQueue<Request> queue;
+import java.util.concurrent.BlockingQueue;
+
+import util.Request;
+
+
 /**
  * @author Samuel Whitty & Said Omar & Everett Soldaat
  *
  */
 public class ElevatorSubsystem {
-	DatagramPacket sendPacket, receivePacket;
-        DatagramSocket sendSocket, receiveSocket;
-	/**
-	 * @param args
-	 */
+	
+	private BlockingQueue<Request> queue;
+	public ElevatorSubsystem(BlockingQueue<Request> q) {
+		this.queue = q;
+	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		// Gonna get to this tomorrow.
@@ -34,58 +37,6 @@ public class ElevatorSubsystem {
 		queue.put(request);
 	}
 	
-	//ignore for now, or forever
-	/*
-	public String validatePacket(byte[] data){
-		//checking if input data is valid since all byte values are less than 127
-		for(int s = 1; s<data.length;s++){
-			if (data[s] > 127 && data[s] < 0){
-				return "invalid packet value";
-			}
-		}
-	//add other stuff
-	}
-	
-	
-	public boolean validateRequest(){
-		{
-			//validates the text sent to the elevator is a valid command
-		}
-			
-		
-	public void exchangeData(){
-		//Make a DatagramPacket for receiving oackets
-		
-		
-		
-		
-		
-		//Receive the data from the sxheduler to control the motor and the doors
-		
-		
-		
-		
-		
-		//try catch for waiting until a diagram packet is received from the socket
-		
-		
-		
-		
-		
-		
-		
-		// process the received datagram packet
-		
-		
-		
-		
-		//make a string assuming we use an array for the values?
-		
-		
-		
-		
 
-	}\*
-}
-		
+}	
 		
