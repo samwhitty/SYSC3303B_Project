5b22bd0c655f3483f65d77201c679bfffb690c43
Samuel Whitty <35856869+samwhitty@users.noreply.github.com>
2020-02-01 19:16:45 -0500

    Update FloorSubsystem.java
    Updated with TimeData and removed some repeated code in run()

--- Group9_Iteration1/src/iteration1/FloorSubsystem.java
+++ Group9_Iteration1/src/iteration1/FloorSubsystem.java
@@ -11,14 +11,15 @@ import java.util.concurrent.BlockingQueue;
 import java.util.regex.MatchResult;
 
 import util.Request;
+import util.TimeData;
 
 /**
- * @author Samuel Whitty
+ * @authors Everett Soldaat & Samuel Whitty
  *
  */
 public class FloorSubsystem implements Runnable {
 
-	private static String time;
+	private static TimeData time;
 	private static int floorNum;
 	private static String direction;
 	private static int destinationFloor;
@@ -26,7 +27,7 @@ public class FloorSubsystem implements Runnable {
 	private BlockingQueue<Request> queue;
 
 	public FloorSubsystem(BlockingQueue<Request> q) {
-		time = "00:00:00:00";
+		time = new TimeData();
 		floorNum = 0;
 		direction = "Up";
 		destinationFloor = 0;
@@ -36,17 +37,9 @@ public class FloorSubsystem implements Runnable {
 
 	@Override
 	public void run() {
-		File file = new File("src/iteration1/input.txt");
-		Scanner s;
 		try {
-			s = new Scanner(file);
-			s.findInLine("(\\d+\\S\\d+\\S\\d+\\S\\d) (\\d) ([a-zA-Z]+) (\\d)");
-			MatchResult result = s.match();
-			time = result.group(1);
-			floorNum = Integer.parseInt(result.group(2));
-			direction = result.group(3);
-			destinationFloor = Integer.parseInt(result.group(4));
-		} catch (FileNotFoundException e) {
+			readInput();
+		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
@@ -66,11 +59,13 @@ public class FloorSubsystem implements Runnable {
 		Scanner s = new Scanner(file);
 		s.findInLine("(\\d+\\S\\d+\\S\\d+\\S\\d) (\\d) ([a-zA-Z]+) (\\d)");
 		MatchResult result = s.match();
-		time = result.group(1);
+		time = new TimeData(result.group(1));
 		floorNum = Integer.parseInt(result.group(2));
 		direction = result.group(3);
 		destinationFloor = Integer.parseInt(result.group(4));
+		s.close();
 	}
+	
 	public void sendRequest(Request request) {
 		
 	}
