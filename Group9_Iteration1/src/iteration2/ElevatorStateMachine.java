816edb92b3ef3f797c62f01f824d3edee470e54c
MichaelEvans6 <michael3vans66@gmail.com>
2020-02-14 20:30:58 -0500

    -implemented WAITING State

--- Group9_Iteration1/src/iteration2/ElevatorStateMachine.java
+++ Group9_Iteration1/src/iteration2/ElevatorStateMachine.java
@@ -4,24 +4,45 @@ public class ElevatorStateMachine {
 	
 	public static enum State{
 		WAITING{
-			@Override public State next(Object[] action){
-				
+			@Override public State next(Object[] data){
+				if(data[2] == (String) "Up") {
+					if((int) data[1] < (int) data[3]) {
+						return STOPPED;
+					}
+					else if((int) data[1] == (int) data[3]) {
+						return STOPPED;
+					}
+					else {
+						return MOVING;
+					}
+				}
+				else if(data[2] == (String) "Down") {
+					if((int) data[1] > (int) data[3]) {
+						return STOPPED;
+					}
+					else if((int) data[1] == (int) data[3]) {
+						return STOPPED;
+					}
+					else {
+						return MOVING;
+					}
+				}
 				return null;
 			}
 		},
 		MOVING{
-			@Override public State next(Object[] action){
+			@Override public State next(Object[] data){
 				return null;
 			}
 		},
 		STOPPED{
-			@Override public State next(Object[] action){
+			@Override public State next(Object[] data){
 				
 				return null;
 			}
 		},;
 
-		public State next(Object[] action) {
+		public State next(Object[] data) {
 			// Leave unimplemented
 			return null;
 		}
