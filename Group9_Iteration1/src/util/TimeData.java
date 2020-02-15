ff3f6e279c3093357c9a2c33d999e19a94036a97
Samuel Whitty <samwhitty@users.noreply.github.com>
2020-02-15 18:23:54 -0500

    added setters

--- Group9_Iteration1/src/util/TimeData.java
+++ Group9_Iteration1/src/util/TimeData.java
@@ -6,7 +6,7 @@ package util;
 /**
  * @author Samuel Whitty
  * 
- * Custom time object.
+ *         Custom time object.
  */
 public class TimeData {
 	private int hour;
@@ -14,41 +14,60 @@ public class TimeData {
 	private int second;
 	private int millisecond;
 	private String timeString;
-	
+
+	public void setHour(int hr) {
+		this.hour = hr;
+	}
+
+	public void setMin(int min) {
+		this.minute = min % 60;
+		this.hour = (int) Math.floor(min/60);
+	}
+
+	public void setSec(int sec) {
+		this.second = sec % 60;
+		this.minute = (int) Math.floor(sec/60);
+	}
+
+	public void setMil(int mil) {
+		this.millisecond = mil % 1000;
+		this.second = (int) Math.floor(mil/60);
+	}
+
 	/*
-	 * Parses a String object in the form "hh:mm:ss.mm.mm"
-	 * into a String[4] object.
+	 * Parses a String object in the form "hh:mm:ss.mm.mm" into a String[4] object.
 	 */
 	public String[] parseTime(String time) {
 		String[] parsedTime = new String[5];
-		
-		parsedTime[0] = time.substring(0,1);
-		parsedTime[1] = time.substring(3,4);
-		parsedTime[2] = time.substring(6,7);
+
+		parsedTime[0] = time.substring(0, 1);
+		parsedTime[1] = time.substring(3, 4);
+		parsedTime[2] = time.substring(6, 7);
 		parsedTime[3] = time.substring(9);
-		
+
 		return parsedTime;
 	}
-	
+
 	public int hour() {
 		return this.hour;
 	}
+
 	public int minute() {
 		return this.minute;
 	}
-	
+
 	public int second() {
 		return this.second;
 	}
-	
+
 	public int millisecond() {
 		return this.millisecond;
 	}
-	
+
 	public String toString() {
 		return timeString;
 	}
-	
+
 	public TimeData() {
 		this.hour = 0;
 		this.minute = 0;
@@ -56,16 +75,16 @@ public class TimeData {
 		this.millisecond = 0;
 		this.timeString = "00;00;00.0";
 	}
-	
+
 	public TimeData(String time) {
 		String[] parsedTime = parseTime(time);
-		
+
 		this.hour = Integer.parseInt(parsedTime[0]);
 		this.minute = Integer.parseInt(parsedTime[1]);
 		this.second = Integer.parseInt(parsedTime[2]);
 		this.millisecond = Integer.parseInt(parsedTime[3]);
 		this.timeString = time;
-		
+
 		parsedTime = null;
 	}
 }
