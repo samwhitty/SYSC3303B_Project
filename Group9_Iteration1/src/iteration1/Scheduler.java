faaee4fd87403f6266ecb118863be3f63a7600c8
MichaelEvans6 <michael3vans66@gmail.com>
2020-02-15 21:55:07 -0500

    -edit

--- Group9_Iteration1/src/iteration1/Scheduler.java
+++ Group9_Iteration1/src/iteration1/Scheduler.java
@@ -24,35 +24,36 @@ public class Scheduler extends Thread {
 	private BlockingQueue<Object[]> to_elevator = null;
 	private BlockingQueue<Object[]> from_floor = null;
 	private BlockingQueue<Object[]> to_floor = null;
-	
+
 	private static SchedulerState state;
-	
-	
+
 	/*
 	 * Constructor for the scheduler.
 	 */
-	public Scheduler(BlockingQueue<Object[]> einQueue, BlockingQueue<Object[]> eoutQueue, BlockingQueue<Object[]> finQueue,
-			BlockingQueue<Object[]> foutQueue, ElevatorSubsystem elev, FloorSubsystem floor) {
+	public Scheduler(BlockingQueue<Object[]> einQueue, BlockingQueue<Object[]> eoutQueue,
+			BlockingQueue<Object[]> finQueue, BlockingQueue<Object[]> foutQueue, ElevatorSubsystem elev,
+			FloorSubsystem floor) {
 		this.from_elevator = einQueue;
 		this.to_elevator = eoutQueue;
 		this.from_floor = finQueue;
 		this.to_floor = foutQueue;
 		this.elevator = elev;
 		this.floor = floor;
-		
+
 		this.state = SchedulerState.WAITFORREQUEST;
 
 	}
+
 	/*
 	 * Getter for Scheduler State.
 	 */
 	public static SchedulerState getSchedulerState() {
 		return state;
 	}
-	
 
 	/*
-	 * This method receives data from the floor subsystem. Also empties the out queue.
+	 * This method receives data from the floor subsystem. Also empties the out
+	 * queue.
 	 */
 	public synchronized void receiveFloorData() {
 		try {
@@ -63,25 +64,27 @@ public class Scheduler extends Thread {
 			e.printStackTrace();
 		}
 	}
-	
+
 	/*
-	 * This method sends data to the floor subsystem, and prints out the data being sent.
+	 * This method sends data to the floor subsystem, and prints out the data being
+	 * sent.
 	 */
 	public synchronized void sendDataToFloor() {
 		System.out.println("Sending data to floor");
 		to_floor.add(data);
-		
+
 	}
 
 	/*
-	 * This method receives data from the elevator subsystem, and prints out the data received.
+	 * This method receives data from the elevator subsystem, and prints out the
+	 * data received.
 	 */
 	public synchronized void receiveElevatorData() {
 
 		try {
 			data = from_elevator.take();
 			from_elevator.clear();
-			
+
 			System.out.println("Received data from Elevator_out_Queue:");
 			System.out.println("Time: " + data[0]);
 			System.out.println("Pasengers picked up from Floor: " + data[1]);
@@ -92,23 +95,23 @@ public class Scheduler extends Thread {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+
 	}
-	
+
 	/*
-	 * This method sends data to the elevator subsystem, and prints out the data sent.
+	 * This method sends data to the elevator subsystem, and prints out the data
+	 * sent.
 	 */
 	public synchronized void sendDataToElevator() {
 
-			to_elevator.add(data);
-			
-			System.out.println("Sending data to Elevator_in_Queue:");
-			System.out.println("Time: " + data[0]);
-			System.out.println("Request Floor: " + data[1]);
-			System.out.println("Request Direction: " + data[2]);
-			System.out.println("Request Destination" + data[3]);	
+		to_elevator.add(data);
+
+		System.out.println("Sending data to Elevator_in_Queue:");
+		System.out.println("Time: " + data[0]);
+		System.out.println("Request Floor: " + data[1]);
+		System.out.println("Request Direction: " + data[2]);
+		System.out.println("Request Destination" + data[3]);
 	}
-	
 
 	/**
 	 * Runs the scheduler.
@@ -116,20 +119,20 @@ public class Scheduler extends Thread {
 	@Override
 	public void run() {
 		System.out.println("Scheduler subsystem running.");
-		
-		while(true) {
+
+		while (true) {
 			if (!from_floor.isEmpty()) {
 				receiveFloorData();
 				state = state.next(data);
 			}
-			
+
 			if (state == SchedulerState.DISPATCHELEVATOR) {
 				sendDataToElevator();
 				state = SchedulerState.WAITFORELEVATOR;
 			}
-			
+
 			while (state == SchedulerState.WAITFORELEVATOR) {
-				if(!from_elevator.isEmpty()) {
+				if (!from_elevator.isEmpty()) {
 					this.receiveElevatorData();
 					this.sendDataToFloor();
 				}
@@ -137,11 +140,9 @@ public class Scheduler extends Thread {
 					state = SchedulerState.WAITFORREQUEST;
 				}
 			}
-			
+
 		}
-		
-		
-		
+
 	}
 
 	/**
@@ -152,12 +153,11 @@ public class Scheduler extends Thread {
 		BlockingQueue<Object[]> to_floor = new ArrayBlockingQueue<>(10);
 		BlockingQueue<Object[]> from_elevator = new ArrayBlockingQueue<>(10);
 		BlockingQueue<Object[]> to_elevator = new ArrayBlockingQueue<>(10);
-		
+
 		FloorSubsystem floor = new FloorSubsystem(from_floor, to_floor);
 		ElevatorSubsystem elevator = new ElevatorSubsystem(from_elevator, to_elevator);
-		
-		Scheduler scheduler = new Scheduler(from_elevator, to_elevator, from_floor, to_floor, elevator,
-				floor);
+
+		Scheduler scheduler = new Scheduler(from_elevator, to_elevator, from_floor, to_floor, elevator, floor);
 
 		new Thread(floor).start();
 		new Thread(elevator).start();
